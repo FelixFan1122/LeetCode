17e7b4fad9dee59cb61b02657dc1ff5298259306
FelixFan <zeus_loveship@126.com>
2019-03-04 16:28:23 +0800

    Add solution to Missing Number

---
+++ MissingNumber/Solution.cs
@@ -0,0 +1,10 @@
+using System.Linq;
+
+namespace LeetCode
+{
+    public class Solution {
+        public int MissingNumber(int[] nums) {
+            return nums.Length * (nums.Length + 1) / 2 - nums.Sum();
+        }
+    }
+}
