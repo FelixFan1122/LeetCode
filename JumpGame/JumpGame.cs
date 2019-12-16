45c4d997236034e3496285df5acaa85e61d08bbd
FelixFan <zeus_loveship@126.com>
2019-12-16 14:41:05 +0800

    Add Jump Game

---
+++ JumpGame/JumpGame.cs
@@ -0,0 +1,48 @@
+using System;
+
+namespace JumpGame
+{
+    public class Solution
+    {
+        public bool CanJump(int[] nums)
+        {
+            return Jump(nums) >= 0;
+        }
+
+        public int Jump(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+            {
+                return -1;
+            }
+
+            if (nums.Length == 1)
+            {
+                return 0;
+            }
+
+            var jumps = 0;
+            var left = 0;
+            var right = 0;
+            while (right < nums.Length - 1)
+            {
+                var max = right;
+                for (var i = left; i <= right; i++)
+                {
+                    max = Math.Max(max, i + nums[i]);
+                }
+
+                if (max == right)
+                {
+                    return -1;
+                }
+
+                jumps++;
+                left = right + 1;
+                right = max;
+            }
+
+            return jumps;
+        }
+    }
+}
