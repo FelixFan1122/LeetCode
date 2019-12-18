bb198e732c8e5fa12d1ee804055d8bcb9bf92673
FelixFan <zeus_loveship@126.com>
2019-12-18 11:20:55 +0800

    Add Linked List Cycle II

---
+++ LinkedListCycleII/LinkedListCycleII.cs
@@ -0,0 +1,42 @@
+using LeetCode;
+
+namespace LinkedListCycleII
+{
+    public class Solution
+    {
+        public ListNode DetectCycle(ListNode head)
+        {
+            if (head == null || head.next == null)
+            {
+                return null;
+            }
+
+            var hare = head.next.next;
+            var tortoise = head.next;
+            while (hare != null && hare != tortoise)
+            {
+                if (hare.next == null)
+                {
+                    break;
+                }
+
+                hare = hare.next.next;
+                tortoise = tortoise.next;
+            }
+
+            if (hare != tortoise)
+            {
+                return null;
+            }
+
+            hare = head;
+            while (hare != tortoise)
+            {
+                hare = hare.next;
+                tortoise = tortoise.next;
+            }
+
+            return hare;
+        }
+    }
+}
