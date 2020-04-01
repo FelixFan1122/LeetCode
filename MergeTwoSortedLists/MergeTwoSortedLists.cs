22636af8857ed1114bbee85ea40e60090da141a5
FelixFan <zeus_loveship@126.com>
2020-04-01 15:48:59 +0800

    Add Merge Two Sorted Lists

---
+++ MergeTwoSortedLists/MergeTwoSortedLists.cs
@@ -0,0 +1,58 @@
+namespace LeetCode.MergeTwoSortedLists
+{
+    public class Solution
+    {
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            if (l1 == null)
+            {
+                return l2;
+            }
+
+            if (l2 == null)
+            {
+                return l1;
+            }
+
+            ListNode merged;
+            if (l1.val < l2.val)
+            {
+                merged = l1;
+                l1 = l1.next;
+            }
+            else
+            {
+                merged = l2;
+                l2 = l2.next;
+            }
+
+            var current = merged;
+            while (l1 != null && l2 != null)
+            {
+                if (l1.val < l2.val)
+                {
+                    current.next = l1;
+                    l1 = l1.next;
+                }
+                else
+                {
+                    current.next = l2;
+                    l2 = l2.next;
+                }
+
+                current = current.next;
+            }
+
+            if (l1 == null)
+            {
+                current.next = l2;
+            }
+            else if (l2 == null)
+            {
+                current.next = l1;
+            }
+
+            return merged;
+        }
+    }
+}
